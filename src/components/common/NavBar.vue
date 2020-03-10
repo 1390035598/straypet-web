54fc7dbd297aaa92566b823c49a72271bb8a7c7d
xiangchengda <1390035598@qq.com>
2020-03-10 10:41:05 +0800

    test

---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/common/NavBarLogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav-bar-logo">
+    <img src="~assets/img/navbar/logo_img_green.png">
+    流浪宠物
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBarLogo'
+}
+</script>
+
+<style scoped>
+
+</style>
